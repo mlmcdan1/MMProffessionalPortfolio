f2c71c353eba1c6e9ff1d1f12dfa39b2377f2f90
mlmcdan1 <77369368+mlmcdan1@users.noreply.github.com>
2022-07-14 18:54:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function openImg(){
     var source = image.src;
     window.open(source);
 }
+
+function openImg2(){
+    var image = document.getElementById('image2');
+    var source = image.src;
+    window.open(source);
+}
